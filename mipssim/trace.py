a61bf5bb6431c0e75777a8a7777c3a8d70975cdc
Julien-Charles L?vesque <levesque.jc@gmail.com>
2013-10-26 15:20:43 -0400

    Chang? le nombre de registres par d?faut, on ne s'en sert pas de toute fa?on.

--- mipssim/trace.py
+++ mipssim/trace.py
@@ -74,24 +74,31 @@ class TextTrace:
                 rs_table.add_row(row)
 
         # Table des registres
-        reg_table = PrettyTable([' '] + [str(a) for a in range(16)])
-        for row_ident in ['%s%d0' % (b, a) for b in ['R', 'F'] for a in range(4)]:
-            padding = ['X'] * 8 if row_ident[1] == '3' else []
-            first_row = ['ROB#'] + \
-              [noneify(simulator.regs.stat[row_ident[:row_ident.index('0')] + str(a)])
-              for a in range(10) if int(row_ident[1:]) + a < 32] + padding
-            reg_table.add_row(first_row)
-            reg_table.add_row([row_ident] + \
-                              [simulator.regs[row_ident[:row_ident.index('0')] + str(a)]
-                              for a in range(10) if int(row_ident[1:]) + a < 32] + \
-                              padding
-                              )
-
+        num_regs = int(len(simulator.regs) / 2)
+        reg_table = PrettyTable([' '] + [str(a) for a in range(10)])
+        for reg_type in ['R', 'F']:
+            for row_start in range(0, num_regs, 10):
+                remainder = num_regs - row_start
+                if remainder < 10:
+                    padding = ['X'] * (10 - remainder)
+                    row_end = row_start + remainder
+                else:
+                    padding = []
+                    row_end = row_start + 10
+                reg_stat_row = ['ROB#'] + [noneify(simulator.regs.stat[reg_type + str(a)])
+                    for a in range(row_start, row_end)] + padding
+                reg_table.add_row(reg_stat_row)
+                
+                reg_value_row = [reg_type + str(row_start)] +\
+                    [simulator.regs[reg_type + str(a)] for a in range(row_start, row_end)] +\
+                    padding
+                reg_table.add_row(reg_value_row)
+                
         # Affichage des tableaux précédemment créés
         self.trace_f.write('%s\n' % ('=' * 80))
         self.trace_f.write('Cycle: %d\n' % simulator.horloge)
         self.trace_f.write('Program Counter : %d\n' % simulator.PC)
-        self.trace_f.write('Stations de réservation:\n%s\n\n' % str(rs_table))
+        self.trace_f.write('Stations de réservation:\n%s\n' % str(rs_table))
         self.trace_f.write('Registres: \n%s\n' % str(reg_table))
         self.trace_f.write('ROB: \n%s\n' % (rob_table))
 
