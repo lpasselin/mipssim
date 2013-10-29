ea41af4c0ff76ca47f2dc3f4263b5913f76f4d20
Julien-Charles L?vesque <levesque.jc@gmail.com>
2013-10-29 15:39:07 -0400

    Chang? les branch pour ?tre une unit? fonctionnelle ind?pendante. Chang? le header de Copyright des fichiers.

--- utils/trace_diff.py
+++ utils/trace_diff.py
@@ -1,5 +1,12 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011-2013, Julien-Charles Lévesque <levesque.jc@gmail.com>
+#  and contributors.
+#
+# Distributed under the terms of the MIT license. See the COPYING file at
+#  the top-level directory of this project and at
+#  https://bitbucket.org/levesque/mipssim/raw/tip/COPYING
+
 '''
 Effectue une comparaison entre deux traces et trouve les différences pour chaque cycle d'horloge.
 '''
