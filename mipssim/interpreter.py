9bd535db2bfd0725ffcef226467e6edf833d520d
Julien-Charles Levesque <levesque.jc@gmail.com>
2014-10-28 11:31:44 -0400

    -Utilisation des vrais registres MIPS -Ajout des op?rations MOVE, LW, NOP -Il manque: le support des directives, l'utilisation de vrais pointeurs m?moire pour les instructions et pour les donn?es (possiblement compliqu?).

--- mipssim/interpreter.py
+++ mipssim/interpreter.py
@@ -13,12 +13,17 @@ import re
 #local imports
 from components import Instruction
 
+DIRECTIVE_SET = {}
+
 # Instruction Set: {'INSTR': ['Unite_fonctionnelle', 'action', 'operator']}
 # $0 = premier argument, $1 = 2e argument, etc.
 INSTRUCTION_SET = {'LD':     ('Load', '$0 = $1'),        # 0. Memory Read
                    'L.D':    ('Load', '$0 = $1'),
+                   'LW':     ('Load', '$0 = $1'),
                    'SD':     ('Store', '$1 = $0'),       # 2. Memory Save
                    'S.D':    ('Store', '$1 = $0'),
+                   'SW':     ('Store', '$1 = $0'),
+                   'MOVE':   ('Move', '$0 = $1'),
                    'ADD.D':  ('Add', '$0 = $1 + $2', '+'),    # 4. Floating point operations
                    'SUB.D':  ('Add', '$0 = $1 - $2', '-'),
                    'MUL.D':  ('Mult', '$0 = $1 * $2', '*'),
@@ -27,6 +32,7 @@ INSTRUCTION_SET = {'LD':     ('Load', '$0 = $1'),        # 0. Memory Read
                    'DADDU':  ('ALU', '$0 = $1 + $2', '+'),
                    'DADDI':  ('ALU', '$0 = $1 + $2', '+'),
                    'DADDIU': ('ALU', '$0 = $1 + $2', '+'),
+                   'ADDIU':  ('ALU', '$0 = $1 + $2', '+'),
                    'DSUB':   ('ALU', '$0 = $1 - $2', '-'),
                    'DSUBU':  ('ALU', '$0 = $1 - $2', '-'),
                    'DMUL':   ('ALU', '$0 = $1 * $2', '*'),
@@ -38,20 +44,20 @@ INSTRUCTION_SET = {'LD':     ('Load', '$0 = $1'),        # 0. Memory Read
                    'BNEZ':   ('Branch', '$2 = $1 if $0 != 0 else $2'),
                    'BEQ':    ('Branch', '$3 = $2 if $0 == $1 else $3'),
                    'BNE':    ('Branch', '$3 = $2 if $0 != $1 else $3'),
-                   'J':      ('Branch', '$1 = $0')
+                   'J':      ('Branch', '$1 = $0'),
+                   'NOP':    ('Nop', '')
                    }
 
-memory_re = re.compile('^-*\d+\([RF][-]?\d+[.]?\d*\)$')
+memory_re = re.compile('^-?\d+\(\$')
 
-
-def interpret_asm(source_file):
+def interpret_asm(simulator, source_file):
     '''
     Interpréteur de source assembleur du MIPS.
 
     Entrée: Source en assembleur MIPS 64 bits.
     Sortie: Une liste. Chaque élément de la liste est une ligne de code.
 
-    Ces éléments sont des tuples de la forme suivante::
+    Ces éléments sont des tuples de la forme suivante:
         (['Unite_fonctionnelle', 'Operation_a_effectuer'], ['Param1', 'Param2', ...])
 
     '''
@@ -61,8 +67,9 @@ def interpret_asm(source_file):
     print('Fichier source lu avec succès!')
 
     # Retrait des caractères de fin de ligne et des commentaires
-    source = list(map(lambda x: x.strip().split(';')[0], source))
-    print(source)
+    # ; et # sont considérés comme des commentaires
+    source = [s.strip().split(';')[0].split('#')[0] for s in source]
+    source = [s for s in source if len(s) != 0]
 
     # Gestion des labels. Après cette opération, les labels sont
     # enlevés de la source
@@ -71,7 +78,7 @@ def interpret_asm(source_file):
 
     # Mapping des opérations dans la table en haut du fichier.
     # Retourne une liste de tuple [(instruction_reference, parametres), ...]
-    source = parse_instructions(source, labels)
+    source = parse_instructions(simulator, source, labels)
     print(source)
 
     return source
@@ -83,18 +90,18 @@ def parse_labels(source):
     la classe sous la forme :
     labels['nom_du_label'] = numero_de_l'operation
     ou
-    {'nom_du_label': numero_de_l'operation, 'nom_du_label_2': numero_de_l'operation_2}
+      {'nom_du_label': numero_de_l'operation,
+       'nom_du_label_2': numero_de_l'operation_2}
     '''
-    # Sectionnement de la source à la première césure de chaque ligne sous
-    # forme de tokens
+    # Sectionnement de chaque ligne sous forme de tokens au premier espace
     # ie. [['Loop:', 'L.D    F0,0(R1)'], ['ADD.D', 'F4,F0,F2'], ['S.D', 'F4,0(R1)'], ['DADDIU', 'R1,R1,#-8'], ['BNE', 'R1,R2,Loop']]
     source = [[token.strip() for token in ligne.split(None, 1)] for ligne in source]
     labels = {}
 
     # Population du dictionnaire des labels
     for index, operation in enumerate(source):
-        # Trouver un label
-        if operation[0][-1] == ':':
+        # Trouver un label, enlever le cas spécial des strings
+        if operation[0][-1] == ':' and len(operation) == 1:
             # Assigner dans le dictionnaire des labels la ligne à laquel
             # ce label est.
             labels[operation[0][:-1]] = index
@@ -108,7 +115,7 @@ def parse_labels(source):
     return [' '.join(a) for a in source], labels
 
 
-def parse_instructions(source, labels):
+def parse_instructions(simulator, source, labels):
     '''
     Convertis une source composée des lignes de codes, labels exclus, en
     une liste de tuples représentant l'instruction puis ses paramètres opaques.
@@ -128,22 +135,38 @@ def parse_instructions(source, labels):
     instructions = []
 
     # Remplacement des labels par les # de ligne. [ format #CHIFFRE pour simplifier l'évaluation ]
+    instr_num = 0
     for line_num, line in enumerate(source):
+        if line == '':
+            continue
+
         elems = line.split()
         operation = elems[0].upper()
-        instr = INSTRUCTION_SET[operation]
 
-        operands = elems[1].split(',')
-        operator = None
-        if len(instr) > 2:
-            operator = instr[2]
+        #Si la ligne débute par un ., il s'agit d'une directive, on peut la
+        # traiter maintenant.
+        if line[0] == '.':
+            #directive = DIRECTIVE_SET[operation]
+            #ignore directives for now, ouch.
+            pass
+        else:
+            instr = INSTRUCTION_SET[operation]
 
-        #Remplace les labels par des # de ligne.
-        for i, o in enumerate(operands):
-            if o in labels.keys():
-                operands[i] = '#' + str(labels[o])
+            if len(elems) > 1:
+                operands = elems[1].split(',')
+                operator = None
+                if len(instr) > 2:
+                    operator = instr[2]
 
-        instructions.append(Instruction(line_num, operation, instr[0], instr[1], operands, operator))
+                #Remplace les labels par des # de ligne.
+                for i, o in enumerate(operands):
+                    if o in labels.keys():
+                        operands[i] = '#' + str(labels[o])
+
+                instructions.append(Instruction(line_num, operation, instr[0], instr[1], operands, operator))
+            else:
+                instructions.append(Instruction(line_num, operation, instr[0], instr[1], [], None))
+            instr_num += 1
 
     return instructions
 
